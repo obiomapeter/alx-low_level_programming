0ac1925a4861459c4950eddf49bd40746423802b
obiomapeter <joynerjuno49@gmail.com>
2022-09-08 18:37:53 -0700

    cisfun

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return always 0 (success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is apiece of art\n");
+    return (0);
+}    
